3c4e22b50b5957da77a496da91a20c2ef04dc6bc
Thithirat <thithirat.chsw@gmail.com>
2022-07-02 17:05:16 +0700

    update

---
+++ Layout.js
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+const Layout = ({children, toggleTheme, currentTheme}) => {
+    return (
+        <div>
+            <Navbar toggleTheme={toggleTheme} currentTheme={currentTheme}/>
+            {children}
+            <Footer currentTheme={currentTheme}/>
+        </div>
+    )
+}
+
+export default Layout
--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['cdn-images-1.medium.com', 'miro.medium.com', 'res.cloudinary.com'],
+  },
 }
