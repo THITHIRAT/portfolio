181bedd6698f65ac10e0b0538d56cc45cb544742
Thithirat <thithirat.chsw@gmail.com>
2022-07-02 17:55:10 +0700

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ['cdn-images-1.medium.com', 'miro.medium.com', 'res.cloudinary.com'],
-  },
 }
